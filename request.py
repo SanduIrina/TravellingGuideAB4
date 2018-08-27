638c103685e9544b013de4c64a1f870fb3f2a9ca
Irina <sandu.irina2@gmail.com>
2018-08-27 15:55:58 +0300

    new update

--- request.py
+++ request.py
@@ -17,6 +17,10 @@ from shapely.geometry import LineString, Polygon, Point
 
 from datetime import datetime
 
+SOLID_YELLOW = map_lane_pb2.LaneBoundaryType.SOLID_YELLOW
+CURB = map_lane_pb2.LaneBoundaryType.CURB
+DOTTED_WHITE = map_lane_pb2.LaneBoundaryType.DOTTED_WHITE
+
 # gmaps = googlemaps.Client(key='AIzaSyDpUQ2C21m2UVSZQegN7A9KRrKF2eJHilA')
 
 # now = datetime.now()
@@ -85,8 +89,8 @@ for i in range(len(newx)):
 	nnewy.append(yi)
 
 points = np.array(list(zip(nnewx,nnewy)))
-plt.plot(newx, newy)
-plt.show()
+# plt.plot(newx, newy)
+# plt.show()
 print(points)
 
 map = map_pb2.Map()
@@ -97,10 +101,16 @@ left_lane_x, right_lane_x, left_lane_y, right_lane_y = laneL.justGetMeTheLanes(p
 left_points = []
 left_points = np.array(list(zip(left_lane_x, left_lane_y)))
 laneL.add(left_points, 30, map_lane_pb2.Lane.NO_TURN, map_lane_pb2.Lane.CITY_DRIVING, map_lane_pb2.Lane.FORWARD, 3.3)
+laneL.add_overlap(1)
+laneL.set_left_lane_boundary_type(CURB, True)
+laneL.set_right_lane_boundary_type(DOTTED_WHITE, True)
 
 right_points = []
 right_points = np.array(list(zip(right_lane_x, right_lane_y)))
 laneR.add(right_points, 30, map_lane_pb2.Lane.NO_TURN, map_lane_pb2.Lane.CITY_DRIVING, map_lane_pb2.Lane.FORWARD, 3.3)
+laneR.add_overlap(2)
+laneR.set_left_lane_boundary_type(DOTTED_WHITE, True)
+laneR.set_right_lane_boundary_type(CURB, True)
 
 map_file = open('try.txt', 'w')
 map_file.write(str(map))
